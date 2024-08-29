6c1c69a6d49894785567c3235880604771f44cc7
Michael James <mickjamesit@gmail.com>
2024-08-29 15:09:17 +1000

    Small-ui-fix

--- components/auth-layout.tsx
+++ components/auth-layout.tsx
@@ -9,14 +9,14 @@ const AuthLayout: React.FC<AuthLayoutProps> = ({ children }) => {
   return (
     <div className="min-h-screen flex flex-col lg:grid lg:grid-cols-2">
       {/* Header Section - Visible only on smaller screens */}
-      <div className="w-full bg-primary py-6 flex justify-center lg:hidden">
+      <div className="w-full bg-primary py-5 flex justify-center lg:hidden">
         <div className="w-[350px] flex items-center justify-center">
           <SiteLogo />
         </div>
       </div>
 
       {/* Children Section - Centered on larger screens */}
-      <div className="flex flex-col items-center justify-center flex-grow py-8 lg:py-0 lg:flex lg:items-center lg:justify-center lg:col-start-1">
+      <div className="flex flex-col items-center justify-center flex-grow  lg:py-0 lg:flex lg:items-center lg:justify-center lg:col-start-1">
         <div className="mx-auto w-[350px] space-y-6">
           <div className="space-y-2">{children}</div>
         </div>
